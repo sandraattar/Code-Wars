8f355e1ef190c88222bb812a76c8db8aca74d5f1
Sandra Attar <39464394+sandraattar@users.noreply.github.com>
2018-08-03 11:56:16 -0400

    Update averageOfArray.js

---
+++ averageOfArray/averageOfArray.js
@@ -0,0 +1,11 @@
+//Write function avg which calculates average of numbers in given list.
+
+
+
+function find_average(array) {
+   var sum=0;
+   for (var i = 0; i < array.length; i++){
+     sum+=array[i]
+    }
+  return (sum/array.length);
+}
